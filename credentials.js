e5dbe32ec330427347666ee7e9c6f07ae3df6be4
Hajime Miyauchi <hajime.miyauchi@gmail.com>
2022-12-18 16:28:06 +0900

    諸々整えた

--- credentials.js
+++
@@ -1,1 +0,0 @@
-var apikey = 'R_b_o94sKLYv-2UMxHb44jDn-GECBjCrra-g0m9GC8s';
--- demo.js
+++ demo.js
@@ -1,59 +1,41 @@
 
+var apikey = 'R_b_o94sKLYv-2UMxHb44jDn-GECBjCrra-g0m9GC8s';
+
 function showGeoJSONData (map) {
-  // Create GeoJSON reader which will download the specified file.
-  // Shape of the file was obtained by using HERE Geocoder API.
-  // It is possible to customize look and feel of the objects.
-  var reader = new H.data.geojson.Reader('aichi.json', {
-    // This function is called each time parser detects a new map object
+  var reader = new H.data.geojson.Reader('aichi.geojson', {
     style: function (mapObject) {
-      // Parsed geo objects could be styled using setStyle method
       if (mapObject instanceof H.map.Polygon) {
+        var opacity = 1.0 / 2000 * parseInt(mapObject.data.JINKO, 0);
+        
         mapObject.setStyle({
-          fillColor: 'rgba(255, 0, 0, 0.5)',
-          strokeColor: 'rgba(0, 0, 255, 0.2)',
-          lineWidth: 3
+          fillColor: 'rgba(255, 0, 0, ' + opacity + ')',
+          strokeColor: 'rgba(255, 0, 0, 0.5)',
+          lineWidth: 1
         });
       }
-    }
+    }, disableLegacyMode: true
   });
 
-  // Start parsing the file
   reader.parse();
 
-  // Add layer which shows GeoJSON data on the map
   map.addLayer(reader.getLayer());
 }
 
-/**
- * Boilerplate map initialization code starts below:
- */
-
-//Step 1: initialize communication with the platform
-// In your own code, replace variable window.apikey with your own apikey
 var platform = new H.service.Platform({
   apikey: window.apikey
 });
 var defaultLayers = platform.createDefaultLayers();
 
-//Step 2: initialize a map - this map is centered over New Delhi
 var map = new H.Map(document.getElementById('map'),
   defaultLayers.vector.normal.map, {
   center: {lat:35.18028, lng:136.90667},
   zoom: 13,
   pixelRatio: window.devicePixelRatio || 1
 });
-// add a resize listener to make sure that the map occupies the whole container
 window.addEventListener('resize', () => map.getViewPort().resize());
 
-//Step 3: make the map interactive
-// MapEvents enables the event system
-// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
 var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-// Create the default UI components
 var ui = H.ui.UI.createDefault(map, defaultLayers);
-
-// Now use the map as required...
-// addCircleToMap(map);
 
 showGeoJSONData(map);
